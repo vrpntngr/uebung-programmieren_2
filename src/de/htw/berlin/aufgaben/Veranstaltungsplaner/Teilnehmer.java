8788eba274b7113f7cfa1804efeb09211fac0efc
vera.peuntinger <vera.peuntinger@innoq.com>
2024-05-07 15:35:38 +0200

    Teilnehmer Klasse für Übungsaufgabe

---
+++ src/de/htw/berlin/aufgaben/Veranstaltungsplaner/Teilnehmer.java
@@ -0,0 +1,14 @@
+package de.htw.berlin.aufgaben.Veranstaltungsplaner;
+
+public class Teilnehmer {
+    // TODO Klassenvariablen (globale Variablen) für vorname, nachname & Geburtsjahr definieren
+
+    // TODO implementiere equals Methode
+
+    // TODO implementiere hashCode Methode
+
+    // TODO Methode zur Ausgabe eines Teilnehmers als String
+    //  (siehe Screenshot in der Aufgabenbeschreibung)
+
+
+}
