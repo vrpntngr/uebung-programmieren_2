9ba4aff9b07ba964a4853de00af6ef1555159d2f
vera.peuntinger <vera.peuntinger@innoq.com>
2024-05-14 12:22:05 +0200

    uebung 2

---
+++ src/de/htw/berlin/uebung/mitarbeiterverwaltung2/Schaltjahr.java
@@ -0,0 +1,16 @@
+package de.htw.berlin.uebung.mitarbeiterverwaltung2;
+
+public class Schaltjahr {
+
+    public static void main(String[] args) {
+        // Ein Schaltjahr ist definiert als ein Jahr, das durch 4 teilbar ist,
+        // aber ansonsten nicht durch 100 teilbar ist,
+        // außer es ist auch durch 400 teilbar.
+
+        // 2001 ein typisches Normaljahr
+        // 1996 ein typisches Schaltjahr
+        // 1900 ein atypisches Normaljahr und
+        // 2000 ein atypisches Schaltjahr
+    }
+
+}
