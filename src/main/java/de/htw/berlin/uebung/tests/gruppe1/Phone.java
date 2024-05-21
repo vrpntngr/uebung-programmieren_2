28423ffa1883864a2e7052b72111dc73f9a11593
vera.peuntinger <vera.peuntinger@innoq.com>
2024-05-21 10:04:36 +0200

    first iteration

--- src/main/java/de/htw/berlin/uebung/tests/gruppe1/Phone.java
+++ src/main/java/de/htw/berlin/uebung/tests/gruppe1/Phone.java
@@ -3,6 +3,15 @@ package de.htw.berlin.uebung.tests.gruppe1;
 public class Phone {
 
     public boolean answer(boolean isMorning, boolean isMom, boolean isAsleep) {
+        if (isMorning == false && isMom == false && isAsleep == false) {
+            return true;
+        }
+        if (isMorning == true && isMom == true && isAsleep == false) {
+            return true;
+        }
+        if (isMorning == false && isMom == true && isAsleep == false) {
+            return true;
+        }
         return false;
     }
 }
